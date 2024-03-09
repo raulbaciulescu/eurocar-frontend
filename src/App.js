fbc6af223da16436aa11952057744743710b26ee
raulbaciulescu <raulbaciulescu@gmail.com>
2024-03-09 12:34:11 +0200

    primereact working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import './App.css';
+import { Button } from 'primereact/button';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <PrimeReactProvider>
+        <div className="App">
+          <Button>
+            Hello
+          </Button>
+        </div>
+      </PrimeReactProvider>
   );
 }
 
